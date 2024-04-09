c6702f9c13713276dd2f2cb65bb7985c380e23b3
Tóth Dániel <tothdaniel561@mailbox.unideb.hu>
2024-04-09 09:36:03 +0200

    database set up

---
+++ src/main/java/com/esen/bookstore/model/Book.java
@@ -0,0 +1,27 @@
+package com.esen.bookstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String publisher;
+    private Double price;
+
+}
